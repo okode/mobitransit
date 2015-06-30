c25c5dd11cb2f579547f6ae0dc31f0dcb0ce4a1d
Ricardo Gil Alcañiz <rgil@okode.com>
2015-06-30 10:48:35 +0200

    Adding utils crypto utils class and using it on the data received by the SUS AP

--- intranet/includes/classes/config.inc.php
+++ intranet/includes/classes/config.inc.php
@@ -5,7 +5,8 @@
  */
 define('HOSTNAME', '127.0.0.1');
 define('USERNAME', 'root');
-define('PASSWORD', 'zxc123');
+#define('PASSWORD', 'zxc123');
+define('PASSWORD', ''); # local config
 define('DATABASE', 'sus_0_1');
 
 define('DATABASE_CONNECTION_WARNING','Maximum concurrent users number has been overcome. Database server can not be accessed temporarily. Please, wait some seconds and try it again.')
--- intranet/index_sus.php
+++ intranet/index_sus.php
@@ -1,27 +1,53 @@
 <?php
 include 'resources/header.php';
 include 'includes/classes/query.class.php';
+include 'includes/classes/utils.class.php';
 
 $query = new query();
-$user = $_GET['user'];
+$data = $_GET['data'];
+$serviceId = $_GET['serviceid'];
+
+$cryptUtil = new SICUCrypt();
+$decrypted = $cryptUtil->decrypt($data);
+$parameters = [];
+parse_str($decrypted, $parameters);
+
+// Enable in order to debug params received from the SUS AP 
+$debug = false;
+
 ?>
 <div id="content" class="content">
 	<div style="width: 100%; height: 30px;">
 		<div id="column_left" class="column_left">
-			Welcome to Mobitransit, the easiest way for transport agencies to reach their users with real-time information through amazing interfaces compatible with any computer or mobile device!
+			Hello <strong><?php echo $parameters['username']?></strong>!,
+		<br/><br/>
+		Welcome to Mobitransit, the easiest way for transport agencies to reach their users with real-time information through amazing interfaces compatible with any computer or mobile device!
+		
+		<?php if ($debug) { ?>
+			<br/><br/>
+			Data: <?php echo $data?>
+			<br/><br/>
+			Data decrypted: <?php echo $decrypted; ?>
+			<br/><br/>
+			Parameters:
+			<pre>
+			<?php print_r($parameters); ?>
+			</pre>
+		<?php } ?>
 		</div>
 	</div>
 	<div id="column_right" class="column_right">
 		<?php
-		$loggedUser = $query->findUserByEmail($user);
-		if ($loggedUser) 
-			echo "<a href=\"configurate.php?user=$user\" class=\"link\">Access your control panel</a>";
-		else 
-			echo "<a href=\"configurate.php?user=$user\" class=\"link\">Sign up now!</a>";
+			$loggedUser = $query->findUserByEmail($user);
+			if ($loggedUser) {
+				echo "<a href=\"configurate.php?user=$user\" class=\"link\">Access your control panel</a>";
+			} else { 
+				echo "<a href=\"configurate.php?user=$user\" class=\"link\">Sign up now!</a>";
+			}
 		?>
 		
 		<?php
-		include 'resources/footer.php';
+			include 'resources/footer.php';
 		?>
 	</div>
 </div>
